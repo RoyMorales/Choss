20cad68795bab9877f24fbfefaaf4e37bdaec5d0
Rui Morais <rui.pinheiro.morais@ua.pt>
2023-03-18 20:26:06 +0000

    Added a new color to selected piece

--- settings.py
+++ settings.py
@@ -3,8 +3,8 @@
 class SettingsClass:
     def __init__(self):
          # Window Settings
-        self.window_width = 800
-        self.window_height = 800
+        self.window_width = 1000
+        self.window_height = 1000
 
         # Game Grid
         self.number_squares = int(8)
@@ -16,7 +16,7 @@ class SettingsClass:
         self.square_colour_dark = (112, 80, 47, 44)
         self.square_colour_move = (240, 135, 29, 94)
         self.square_colour_piece = (204, 205, 210, 81)
-        self.square_colour_highlight = (210, 62, 53, 82)
+        self.square_colour_highlight = (210, 62, 53, 85)
         # Grid Lines
         self.lines_colour = (112, 63, 13, 44)
         self.lines_width = 6
